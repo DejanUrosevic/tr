10893d002a2fe15b84fe64ad097895b33ab5481e
DejanUrosevic <dejansuca@gmail.com>
2018-02-17 12:25:27 +0100

    Small changes.

--- src/components/LogIn/LogInForm.jsx
+++ src/components/LogIn/LogInForm.jsx
@@ -1,49 +1,4 @@
 import React from 'react';
-/*import { Form, Icon, Input, Button, Checkbox } from 'antd';
-import { LogInComponent } from '../../containers/LogInComponent';
-const FormItem = Form.Item
-
-const LogInForm = ({ }) => {
-    const { getFieldDecorator } = this.props.form;
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-            if (!err) {
-                console.log('Received values of form: ', values);
-            }
-        });
-    }
-
-    return (
-        <div>
-            <Form onSubmit={this.handleSubmit} className="login-form">
-                <FormItem>
-                    {getFieldDecorator('userName', {
-                        rules: [{ required: true, message: 'Please input your username!' }],
-                    })(
-                        <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-                        )}
-                </FormItem>
-                <FormItem>
-                    {getFieldDecorator('password', {
-                        rules: [{ required: true, message: 'Please input your Password!' }],
-                    })(
-                        <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-                        )}
-                </FormItem>
-                <FormItem>
-                    <Button type="primary" htmlType="submit" className="login-form-button">
-                        Log in
-                    </Button>
-                </FormItem>
-            </Form>
-        </div>
-    )
-}
-
-const WrappedNormalLoginForm = Form.create()(LogInComponent);
-export default WrappedNormalLoginForm; */
-//import 'antd/dist/antd.css';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
 const FormItem = Form.Item;
 
@@ -78,14 +33,10 @@ class NormalLoginForm extends React.Component {
           {getFieldDecorator('remember', {
             valuePropName: 'checked',
             initialValue: true,
-          })(
-            <Checkbox>Remember me</Checkbox>
-          )}
-          <a className="login-form-forgot" href="">Forgot password</a>
+          })}
           <Button type="primary" htmlType="submit" className="login-form-button">
             Log in
           </Button>
-          Or <a href="">register now!</a>
         </FormItem>
       </Form>
     );
--- src/containers/LogInComponent.jsx
+++ src/containers/LogInComponent.jsx
@@ -1,27 +1,43 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import LogInForm from '../components/LogIn/LogInForm';
+import Footer from '../components/footer/footer';
+
 
 class LogInComponent extends React.Component {
-  static propTypes = {
-    prop: PropTypes
-  }
+
 
   render() {
     return (
-      <div>
-          HelloWorld!!!
+      <div className="b1" style={{overflow: 'hidden'}}>
+       <div styles="{{overflow:hidden;}}">
+          <header className="header" id="header">
+            <div className="container">
+              <figure className="logo animated fadeInDown delay-07s">
+                <img src="/images/Transion_logo.png" className="img-circle img-responsive" alt="" />
+              </figure>
+              <h1 className="animated fadeInDown delay-07s">Welcome To <span style={{color:'#42cbf4'}}>Transion </span></h1>
+            </div>
+          </header>
+        </div>
+
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <LogInForm />
+        </div>
+
+        <Footer />
       </div>
     )
   }
 }
 
 const mapStateToProps = (state) => ({
-  
+
 });
 
 const mapDispatchToProps = {
-  
+
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(LogInComponent);
